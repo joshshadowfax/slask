c6e4326fcb8deb37be1a4e078f578c76a6d51293
joshshadowfax <joshshadowfax@gmail.com>
2015-08-25 01:12:13 -0700

    You know why

--- plugins/amazing.py
+++ plugins/amazing.py
@@ -6,7 +6,7 @@ amazing = "http://www.tsm.gg/wp-content/uploads/2014/07/amazing-200x200.jpg"
 
 def on_message(msg, server):
     text = msg.get("text", "")
-    match = re.findall(r"!amazing", text)
+    match = re.findall(r"amazing", text)
     if not match: return
 
     return amazing
